8db8d109e10218c0f38f9ae68c55ab1855ffa89c
SimonDahrs <s.dahrs@gmail.com>
2024-08-28 22:24:44 +0200

    Rename dist to docs

--- frontend/personal-website/vite.config.ts
+++ frontend/personal-website/vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "../../dist"
+    outDir: "../../docs"
   },
   base: process.env.NODE_ENV === "production" ? "/SimonDahrs/" : "",
   plugins: [

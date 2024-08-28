106ba1eafcda19fd1765242b960104b9b18e3942
SimonDahrs <s.dahrs@gmail.com>
2024-08-28 22:19:36 +0200

    Set public base path

--- frontend/personal-website/vite.config.ts
+++ frontend/personal-website/vite.config.ts
@@ -7,9 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "../.."
-  },
+  base: process.env.NODE_ENV === "production" ? "/SimonDahrs/" : "",
   plugins: [
     vue(),
     vueJsx(),

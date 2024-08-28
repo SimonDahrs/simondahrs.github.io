c94e9672cfbba0be9a5d13f8c119969e6933f6f0
SimonDahrs <s.dahrs@gmail.com>
2024-08-28 22:12:33 +0200

    Add frontend build

--- frontend/personal-website/vite.config.ts
+++ frontend/personal-website/vite.config.ts
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "../.."
+  },
   plugins: [
     vue(),
     vueJsx(),
